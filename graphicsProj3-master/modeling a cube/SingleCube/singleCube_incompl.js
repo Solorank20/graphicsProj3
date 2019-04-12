81b4e6928d79c2a0f58c187e2e20139e21ed0ffd
Solorank20 <37387835+Solorank20@users.noreply.github.com>
2019-04-12 08:30:27 -0400

    Add files via upload

--- graphicsProj3-master/modeling a cube/SingleCube/singleCube_incompl.js
+++ graphicsProj3-master/modeling a cube/SingleCube/singleCube_incompl.js
@@ -10,10 +10,17 @@ var vertices;
 var faces;
 var colors;
 var cube;
-var groundSize = 800;
 
+var turnStile;
+
+var nintyDegrees = Math.PI/2;
+var turnStileSize = 160;
+var groundSize = 800;
 var blockWidth = 40;
-var firstCube;
+var firstCubeMesh;
+var leftWall;
+var rightWall;
+var all;
 
 init();
 draw();
@@ -30,18 +37,15 @@ function init(){
 	renderer.setSize(canvasWidth, canvasHeight);
 
 	document.getElementById("WebGLCanvas").appendChild(renderer.domElement);
-
+	document.addEventListener("keydown", onDocumentKeyDown, true);
 	scene = new THREE.Scene();
 
 	viewLength = 500;
 	aspRat = canvasWidth/canvasHeight;
 
-	// Create a PerspectiveCamera with a FOV of 45, aspect ratio of aspRat,
-	// 		a new plane of 0.1 and a far plane of 1000
-	//	Set the camera at 100, 200, 300 and make it look at 0, 0, 0
-	// 	Add the camera to the scene
+
 	camera = new THREE.PerspectiveCamera(45, aspRat, .1 , 1500)
-	camera.position.set(600,400,600);
+	camera.position.set(00,400,600);
 	camera.lookAt(0,0,0);
  	scene.add(camera);
 }
@@ -51,6 +55,8 @@ function draw(){
 	buildGround();
 	buildFirstCube();
 	buildTurnStile();
+	buildWall(0);
+	buildWall(1);
 
 }
 
@@ -69,26 +75,28 @@ function initGeom(){
 
 	// Create an array of Face3 objects out of the vertices stored in the array
 	//		above and assigns the array to the faces global variable
-	 faces = [new THREE.Face3(0,1,2), new THREE.Face3(0,2,3), new THREE.Face3(0,1,4),
- 							new THREE.Face3(1,5,4), new THREE.Face3(1,5,6), new THREE.Face3(6,2,1),
-						  new THREE.Face3(0,4,7), new THREE.Face3(7,3,0), new THREE.Face3(3,2,6),
-						  new THREE.Face3(7,3,2), new THREE.Face3(4,5,6), new THREE.Face3(6,7,4)];
+	 faces = [new THREE.Face3(0,1,2), new THREE.Face3(0,2,3),
+		 new THREE.Face3(0,1,4), new THREE.Face3(1,5,4),
+		 new THREE.Face3(1,5,6), new THREE.Face3(6,2,1),
+		 new THREE.Face3(0,4,7), new THREE.Face3(7,3,0),
+		 new THREE.Face3(3,2,6), new THREE.Face3(7,3,6),
+		 new THREE.Face3(4,5,6), new THREE.Face3(4,6,7)];
 
 	// Create an array of colors. Each side of the cube should have one color.
-var colors = [new THREE.Color(0x0000ff),new THREE.Color(0xD3D3D3),new THREE.Color(0xaa3333),
-							new THREE.Color(0x00ff00),new THREE.Color(0x222222),new THREE.Color(0xffffff)];
+ colors = [new THREE.Color(0xD4EDF4),new THREE.Color(0xE2F2D5),new THREE.Color(0xf9fbba),
+							new THREE.Color(0xf6c2c2),new THREE.Color(0xebebeb),new THREE.Color(0xece6e3)];
 }
 
 function buildFirstCube(){
 
 	// Create a new THREE.Geometry and assigns it to the geom global variable
-  firstCube = new THREE.Geometry();
+  var firstCube = new THREE.Geometry();
 	// Assign the array of vertices to geom's vertices property
 	firstCube.vertices = vertices;
 	// Assign the array of faces to geom's faces property
 	firstCube.faces = faces;
 	// Set the color property of the faces array with color's setRGB method
-	var colorIndex;
+	var colorIndex =0;
 	for(var i = 0; i < firstCube.faces.length; i+=2){
 		var face = firstCube.faces[i];
         face.color = colors[colorIndex];
@@ -96,50 +104,102 @@ function buildFirstCube(){
         face.color = colors[colorIndex];
         colorIndex+= 1;
 	}
-	var cubeMat = new THREE.MeshBasicMaterial({color: 0xff0000, side: THREE.DoubleSide})
-	var mesh = new THREE.Mesh(firstCube,cubeMat);
-	mesh.position.set(-groundSize/4,20,0);
+	var cubeMat = new THREE.MeshBasicMaterial({vertexColors:THREE.FaceColors, side: THREE.DoubleSide})
+	firstCubeMesh = new THREE.Mesh(firstCube, cubeMat);
+	firstCubeMesh.position.set(-260,blockWidth/2,0);
+
+
 	// Create a new meesh based on the face color and assign the mesh to the
 	//	global variable tempGeom
-	scene.add(mesh);
 }
 
-function buildwall(side){
+function buildWall(side){
+		if(side ==0){
+			var leftWall = new THREE.Object3D();
+			var moveCubex = 0;
+			var moveCubey = 0;
+  	for(var j = 0; j <4 ; j++){
+				for(var i = 0; i < 5; i++){
+						var cube = firstCubeMesh.clone();
+						cube.position.set(-240 + moveCubex, blockWidth/2 + moveCubey, 0);
+						if((i %2 == 0 && j % 2 != 0) || (j == 0 || j==2) && i %2 != 0){
+							cube.rotation.y = nintyDegrees;
+							cube.rotation.x = nintyDegrees;
+						}
+						leftWall.add(cube);
+						moveCubex += blockWidth;
 
+					}
+					moveCubex =0;
+					moveCubey += blockWidth;
+				}
+			scene.add(leftWall)
+		}else{
+			var rightWall = new THREE.Object3D();
+			var moveCubex = 0;
+			var moveCubey = 0;
+		for(var j = 0; j <4 ; j++){
+				for(var i = 0; i < 5; i++){
+						var cube = firstCubeMesh.clone();
+						cube.position.set(240 - moveCubex, blockWidth/2 + moveCubey, 0);
+						if((i %2 == 0 && j % 2 != 0) || (j == 0 || j==2) && i %2 != 0){
+							cube.rotation.y = nintyDegrees;
+							cube.rotation.x = nintyDegrees;
+						}
+						rightWall.add(cube);
+						moveCubex += blockWidth;
+					}
+					moveCubex =0;
+					moveCubey += blockWidth;
+				}
+		scene.add(rightWall);
+	}
 }
 
 function buildTurnStile(){
+	turnStile = new THREE.Object3D();
+
+	//build middle pole.
 	var cylGeom = new THREE.CylinderGeometry(5,5,170,5);
 	var cylMat = new THREE.MeshBasicMaterial({color: 0x000000});
 	var cylinder = new THREE.Mesh(cylGeom, cylMat);
 	cylinder.position.set(0,85,0);
 
-	var geometry = new THREE.BoxGeometry( 10, 160, 80 );
-  var material = new THREE.MeshBasicMaterial( {color: 0xC0C0C0} );
-  var cube = new THREE.Mesh( geometry, material );
+	//build first wall
+	var turnGeo = new THREE.BoxGeometry( 10, turnStileSize, blockWidth*2 );
+  var turnMat = new THREE.MeshBasicMaterial( {color: 0xC0C0C0} );
+  var turnWall = new THREE.Mesh( turnGeo, turnMat );
+	turnWall.position.set(0,turnStileSize/2 + 5 ,0);
 
-	cube.position.set(0,85,0)
-	var cube2 = cube.clone();
-	cube2.rotation.y =  Math.PI / 2;
-
-  scene.add( cube );
-	scene.add(cube2);
+	//clone second wall from first and rotate
+	var turnWall2 = turnWall.clone();
+	turnWall2.rotation.y = nintyDegrees
+	turnStile.add(turnWall);
+	turnStile.add(turnWall2);
+	scene.add(turnStile);
 	scene.add(cylinder);
 }
-
+/*
+*Function for building ground and adding it to the scene.
+*/
 function buildGround(){
 
-	// Create a ground for the cube out of PlaneGeometry and assign it the
-	// 	global ground variable
-	// Position the ground as necessary
 	var planeGeo = new THREE.PlaneGeometry(groundSize,groundSize);
 	var planMat = new THREE.MeshBasicMaterial({color: 0x00ff00, side: THREE.DoubleSide})
 	var plane = new THREE.Mesh(planeGeo, planMat);
-	plane.rotation.x = Math.PI / 2;
+	plane.rotation.x = nintyDegrees;
 	scene.add(plane);
 
-}
 
+}
+function onDocumentKeyDown(event){
+	if(event.keyCode == 81){
+		turnStile.rotation.y += .04;
+		renderScene();
+	}else if (key == 'r') {
+
+	}
+}
 function renderScene(){
 
  	renderer.render(scene, camera);
